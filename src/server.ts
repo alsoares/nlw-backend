34e0be9a73d5ce70f2cee7fcfe896ba33096991c
alsoares <andresoaresti@gmail.com>
2020-06-01 11:14:38 -0300

    add dependencias de typescript

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,18 @@
 import express from 'express';
 
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'André',
+        'Luiz',
+        'Rodrigues',
+        'Soares'
+    ]);
+});
+
+app.listen(3333,() => {
+    console.log('Rodando em http://localhost:3333/');
+});
