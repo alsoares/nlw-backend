da8603894c88bdab644f63bbfc1e1b9ae5b291af
alsoares <andresoaresti@gmail.com>
2020-06-03 19:49:01 -0300

    fix image url

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ export class ItemsController {
             return { 
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/upload/${item.image}`
+                image_url: `http://localhost:3333/uploads/${item.image}`
             }
         })
         
